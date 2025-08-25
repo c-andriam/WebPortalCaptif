95fb3f735639b980a7177a22ce0334ca00539afc
candriam.42 <candriam@student.42antananarivo.mg>
2025-08-25 16:39:29 +0300

    Fix toast component errors

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -85,7 +85,7 @@ const ToastClose = React.forwardRef<
     {...props}
   >
     <X className="h-4 w-4" />
-  </ToastClose>
+  </ToastPrimitives.Close>
 ))
 ToastClose.displayName = ToastPrimitives.Close.displayName
 
@@ -128,5 +128,3 @@ export {
   ToastClose,
   ToastAction,
 }
-
-export { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport }
